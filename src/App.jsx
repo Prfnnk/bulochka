369f53ebfdda7e5b9d1f5e38f256ec865bbf78a4
Masha <parfenenkoma@gmail.com>
2020-03-26 23:43:08 +0300

    add react project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Greeting} from './components/Greeting';
+
+const App = () => {
+  return (
+    <Greeting/>
+     
+  );
+}
+
+export default App;
---
+++ src/components/Greeting.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Hello = styled.div `
+font-size: 60px;
+`;
+
+
+export const Greeting = () => {
+    return(
+        <Hello>Приветики, Артурчик!!!!!!</Hello>
+    )
+}
